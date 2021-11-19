95247634a36620da90642b316507b5fa9c38784f
Eduardo Oliveira <edusmrs@hotmail.com>
2021-11-18 21:45:12 -0300

    Altereção CidadeForm e CidadeList

--- app/control/pessoas/CidadeList.php
+++ app/control/pessoas/CidadeList.php
@@ -12,6 +12,7 @@ use Adianti\Widget\Datagrid\TPageNavigation;
 use Adianti\Widget\Form\TEntry;
 use Adianti\Widget\Form\TLabel;
 use Adianti\Widget\Util\TDropDown;
+use Adianti\Widget\Wrapper\TDBUniqueSearch;
 use Adianti\Wrapper\BootstrapDatagridWrapper;
 use Adianti\Wrapper\BootstrapFormBuilder;
 
@@ -31,7 +32,8 @@ class CidadeList extends TPage
 
         $this->setDatabase('db_condominio');
         $this->setActiveRecord('Cidade');
-        $this-> setDefaultOrder('id', 'asc');
+        $this->setDefaultOrder('id', 'asc');
+        $this->setOrderCommand('estado->nome','(SELECT nome FROM estado WHERE id=cidade.estado_id');
         $this->setLimit(10);
 
         $this->addFilterField('id', '=', 'id');
@@ -45,7 +47,9 @@ class CidadeList extends TPage
         $id = new TEntry('id');
         $nome = new TEntry('nome');
         $codigo_ibge = new TEntry('codigo_ibge');
-        $estado_id = new TEntry('estado_id');
+        $estado_id = new TDBUniqueSearch('estado_id', 'db_condominio', 'Estado', 'id', 'uf');
+        $estado_id->setMinLength(0);
+        $estado_id->setMask('{nome} ({uf})');
 
         $this->form->addFields([ new TLabel('Id') ], [ $id]);
         $this->form->addFields([ new TLabel('Nome') ], [ $nome]);
@@ -65,17 +69,18 @@ class CidadeList extends TPage
         //Criar as colunas
         $column_id = new TDataGridColumn('id', 'Id', 'center', '10%');
         $column_nome = new TDataGridColumn('nome', 'Nome', 'left');
-        $column_cidade_id = new TDataGridColumn('cidade_id', 'Cidade_id', 'center', '10%');
         $column_codigo_ibge = new TDataGridColumn('codigo_ibge', 'Codigo_ibge', 'center', '10%');
+        $column_estado_id = new TDataGridColumn('{estado->nome} ({estado->uf})', 'Estado_id', 'center', '10%');
 
         $this->datagrid->addColumn($column_id);
         $this->datagrid->addColumn($column_nome);
-        $this->datagrid->addColumn($column_cidade_id);
         $this->datagrid->addColumn($column_codigo_ibge);
+        $this->datagrid->addColumn($column_estado_id);
+        
 
         $column_id->setAction(new TAction([$this, 'onReload']), ['order' => 'id']);
         $column_nome->setAction(new TAction([$this, 'onReload']), ['order' => 'nome']);
-        $column_cidade_id->setAction(new TAction([$this, 'onReload']), ['order' => 'cidade_id']);
+        $column_estado_id->setAction(new TAction([$this, 'onReload']), ['order' => 'estado_id']);
         $column_codigo_ibge->setAction(new TAction([$this, 'onReload']), ['order' => 'codigo_ibge']);
 
         $action1 = new TDataGridAction(['CidadeForm', 'onEdit'], ['id' => '{id}', 'register_state' => 'false']);
