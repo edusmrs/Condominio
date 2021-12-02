38a22c296581858f05f22018fc43ffd674de394e
Eduardo Oliveira <edusmrs@hotmail.com>
2021-12-01 21:58:14 -0300

    Criação do UnidadeForm, UnidadeList e Unidade(Model) (INACABADO). Término do EleiçãoForm e EleiçãoList

--- app/control/pessoas/UnidadeList.php
+++ app/control/pessoas/UnidadeList.php
@@ -17,7 +17,7 @@ use Adianti\Widget\Wrapper\TDBUniqueSearch;
 use Adianti\Wrapper\BootstrapDatagridWrapper;
 use Adianti\Wrapper\BootstrapFormBuilder;
 
-class EleicaoList extends TPage
+class UnidadeList extends TPage
 {
     protected $form;
     protected $datagrid;
@@ -32,55 +32,61 @@ class EleicaoList extends TPage
         parent::__construct();
 
         $this->setDatabase('db_condominio');
-        $this->setActiveRecord('Eleicao');
+        $this->setActiveRecord('Unidade');
         $this->setDefaultOrder('id', 'asc');
-        $this->setOrderCommand('pessoa->nome','(SELECT nome FROM pessoa WHERE id=eleicao.pessoa_id');
-        $this->setOrderCommand('papel->nome','(SELECT nome FROM papel WHERE id=eleicao.papel_id');
+        $this->setOrderCommand('pessoa->nome','(SELECT nome FROM pessoa WHERE id=unidade.pessoa_id');
+        //$this->setOrderCommand('papel->nome','(SELECT nome FROM papel WHERE id=unidade.papel_id');
+        //$this->setOrderCommand('grupo->nome','(SELECT nome FROM grupo WHERE id=unidade.grupo_id');
         $this->setLimit(10);
 
         $this->addFilterField('id', '=', 'id');
+        $this->addFilterField('descricao', 'like', 'descricao');
         $this->addFilterField('pessoa_id', '=', 'pessoa_id');
+        $this->addFilterField('bloco', '=', 'bloco');
         $this->addFilterField('papel_id', '=', 'papel_id');
-        /*$this->addFilterField('data_inicio', 'like', 'data_inicio', function($value) {
-            return TDate::convertToMask($value, 'dd/mm/yyyy', 'yyyy-mm-dd');
-        }); // filterField, operator, formField, transformFunction */
+        $this->addFilterField('grupo_id', '=', 'grupo_id');
+        $this->addFilterField('fracao', '=', 'fracao');
         
-        /* $this->addFilterField('data_fim', 'like', 'data_fim', function($value) {
-            return TDate::convertToMask($value, 'dd/mm/yyyy', 'yyyy-mm-dd');
-        }); */
 
-        $this->addFilterField('data_inicio', '=', 'data_inicio');
-        $this->addFilterField('data_fim', '=', 'data_fim');
-        $this->addFilterField('observacao', 'like', 'observacao');
-
-        $this->form = new BootstrapFormBuilder('form_search_Eleicao');
-        $this->form->setFormTitle('Eleicao');
+        $this->form = new BootstrapFormBuilder('form_search_Unidade');
+        $this->form->setFormTitle('Unidade');
 
         $id = new TEntry('id');
+        $descricao = new TEntry('descricao');
         $pessoa_id = new TDBUniqueSearch('pessoa_id', 'db_condominio', 'Pessoa', 'id', 'nome');
+        $bloco = new TEntry('bloco');
         $papel_id = new TDBUniqueSearch('papel_id', 'db_condominio', 'Papel', 'id', 'nome');
-        $data_inicio = new TDate('data_inicio');
-        $data_fim = new TDate('data_fim');
-        $observacao = new TEntry('observacao');
+        $grupo_id = new TDBUniqueSearch('grupo_id', 'db_condominio', 'Grupo', 'id', 'nome');
+        /*$fracao = new TEntry('fracao');
+        $area_util = new TDate('area_util');
+        $area_total = new TDate('area_total');
+        $observacao = new TEntry('observacao');*/
         
 
         $this->form->addFields([ new TLabel('Id') ], [ $id]);
+        $this->form->addFields([ new TLabel('Descrição') ], [$descricao]);
         $this->form->addFields([ new TLabel('Pessoa') ], [$pessoa_id]);
         $this->form->addFields([ new TLabel('Papel') ], [ $papel_id]);
-        $this->form->addFields([ new TLabel('Início Mandato') ], [$data_inicio]);
-        $this->form->addFields([ new TLabel('Término Mandato') ], [$data_fim]);
-        $this->form->addFields([ new TLabel('Observação') ], [$observacao]);
+        $this->form->addFields([ new TLabel('Grupo') ], [ $grupo_id]);
+        /*$this->form->addFields([ new TLabel('Fração') ], [$fracao]);
+        $this->form->addFields([ new TLabel('Área útil') ], [$area_util]);
+        $this->form->addFields([ new TLabel('Área total') ], [$area_total]);
+        $this->form->addFields([ new TLabel('Observação') ], [$observacao]);*/
 
-        $data_inicio->setMask('dd/mm/yyyy');
-        $data_inicio->setDatabaseMask('yyyy-mm-dd');
-        $data_fim->setMask('dd/mm/yyyy');
-        $data_fim->setDatabaseMask('yyyy-mm-dd');
+        $id->setSize('30%');
+        $descricao->setSize('100%');
+        $pessoa_id->setSize('100%');
+        $bloco->setSize('100%');
+        $papel_id->setSize('100%');
+        $grupo_id->setSize('100%');
+       
+
 
         $this->form->setData( TSession::getValue(__CLASS__.'_filter_data') );
 
         $btn = $this->form->addAction(_t('Find'), new TAction([ $this, 'onSearch']), 'fa:search');
         $btn->class = 'btn btn-sm btn-primary';
-        $this->form->addActionLink(_t('New'), new TAction(['EleicaoForm', 'onEdit'], ['register_state' => 'false']), 'fa:plus green');
+        $this->form->addActionLink(_t('New'), new TAction(['UnidadeForm', 'onEdit'], ['register_state' => 'false']), 'fa:plus green');
 
         //Cria datagrid
         $this->datagrid = new BootstrapDatagridWrapper(new TDataGrid);
@@ -90,38 +96,49 @@ class EleicaoList extends TPage
 
         //Criar as colunas
         $column_id = new TDataGridColumn('id', 'Id', 'left', '10%');
+        $column_descricao = new TDataGridColumn('descricao', 'Descrição', 'left', '10%');
         $column_pessoa_id = new TDataGridColumn('{pessoa->nome}', 'Pessoa', 'left');
+        $column_bloco = new TDataGridColumn('bloco', 'Bloco', 'left');
         $column_papel_id = new TDataGridColumn('{papel->nome}', 'Papel', 'left');
-        $column_data_inicio = new TDataGridColumn('data_inicio', 'Início Mandato', 'left');
-        $column_data_fim = new TDataGridColumn('data_fim', 'Término Mandato', 'left');
+        $column_grupo_id = new TDataGridColumn('{grupo->nome}', 'Grupo', 'left');
+        $column_fracao = new TDataGridColumn('fracao', 'Fração', 'left');
+        $column_area_util = new TDataGridColumn('area_util', 'Área útil', 'left');
+        $column_area_total = new TDataGridColumn('area_total', 'Área total', 'left');
         $column_observacao = new TDataGridColumn('observacao', 'Observação', 'left');
 
         $this->datagrid->addColumn($column_id);
+        $this->datagrid->addColumn($column_descricao);
         $this->datagrid->addColumn($column_pessoa_id);
+        $this->datagrid->addColumn($column_bloco);
         $this->datagrid->addColumn($column_papel_id);
-        $this->datagrid->addColumn($column_data_inicio);
-        $this->datagrid->addColumn($column_data_fim);
+        $this->datagrid->addColumn($column_fracao);
+        $this->datagrid->addColumn($column_area_util);
+        $this->datagrid->addColumn($column_area_total);
         $this->datagrid->addColumn($column_observacao);
-        
+
+        $format_value = function($value) {
+            if (is_numeric($value)) {
+                return number_format($value, 2, ',', '.');
+            }
+            return $value;
+        };
+
+        $column_fracao->setTransformer($format_value);
+        $column_area_util->setTransformer($format_value);
+        $column_area_total->setTransformer($format_value);
 
         $column_id->setAction(new TAction([$this, 'onReload']), ['order' => 'id']);
+        $column_id->setAction(new TAction([$this, 'onReload']), ['order' => 'descricao']);
         $column_pessoa_id->setAction(new TAction([$this, 'onReload']), ['order' => 'pessoa_id']);
+        $column_bloco->setAction(new TAction([$this, 'onReload']), ['order' => 'bloco']);
         $column_papel_id->setAction(new TAction([$this, 'onReload']), ['order' => 'papel_id']);
-        $column_data_inicio->setAction(new TAction([$this, 'onReload']), ['order' => 'data_inicio']);
-        $column_data_fim->setAction(new TAction([$this, 'onReload']), ['order' => 'data_fim']);
+        $column_grupo_id->setAction(new TAction([$this, 'onReload']), ['order' => 'grupo_id']);
+        $column_fracao->setAction(new TAction([$this, 'onReload']), ['order' => 'fracao']);
+        $column_area_util->setAction(new TAction([$this, 'onReload']), ['order' => 'area_util']);
+        $column_area_total->setAction(new TAction([$this, 'onReload']), ['order' => 'area_total']);
         $column_observacao->setAction(new TAction([$this, 'onReload']), ['order' => 'observacao']);
 
-        $column_data_inicio->setTransformer( function($value, $object, $row) {
-            $date = new DateTime($value);
-            return $date->format('d/m/Y');
-        });
-
-        $column_data_fim->setTransformer( function($value, $object, $row) {
-            $date = new DateTime($value);
-            return $date->format('d/m/Y');
-        });
-
-        $action1 = new TDataGridAction(['EleicaoForm', 'onEdit'], ['id' => '{id}', 'register_state' => 'false']);
+        $action1 = new TDataGridAction(['UnidadeForm', 'onEdit'], ['id' => '{id}', 'register_state' => 'false']);
         $action2 = new TDataGridAction([$this, 'onDelete'], ['id' => '{id}']);
 
         $this->datagrid->addAction($action1, _t('Edit'), 'fa:edit blue');
