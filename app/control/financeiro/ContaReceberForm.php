84a812a5313cd6ce8565e9593d817458d96e415d
Eduardo Oliveira <edusmrs@hotmail.com>
2021-12-13 21:45:40 -0300

    Ajustes do ContaPagarList, criação e término do ContaReceber/View/List e exportação do banco db_condominio

--- app/control/financeiro/ContaReceberForm.php
+++ app/control/financeiro/ContaReceberForm.php
@@ -9,7 +9,7 @@ use Adianti\Widget\Form\TEntry;
 use Adianti\Widget\Form\TLabel;
 use Adianti\Wrapper\BootstrapFormBuilder;
 
-class ContaPagarForm extends TPage
+class ContaReceberForm extends TPage
 {
     protected $form;
 
@@ -20,74 +20,71 @@ class ContaPagarForm extends TPage
         parent::__construct();
 
         parent::setTargetContainer('adianti_right_panel');
-        $this->setAfterSaveAction(new TAction(['ContaPagarList', 'onReload'], ['register_state' => 'true']) );
+        $this->setAfterSaveAction(new TAction(['ContaReceberList', 'onReload'], ['register_state' => 'true']) );
 
         $this->setDatabase('db_condominio');
-        $this->setActiveRecord('ContaPagar');
+        $this->setActiveRecord('ContaReceber');
 
-        $this->form = new BootstrapFormBuilder('form_Conta_Pagar');
-        $this->form->setFormTitle('Conta a Pagar');
+        $this->form = new BootstrapFormBuilder('form_Conta_Receber');
+        $this->form->setFormTitle('Conta a Receber');
         $this->form->setClientValidation(true);
         $this->form->setColumnClasses(2, ['col-sm-5 col-lg-4', 'col-sm-7 col-lg-8']);
 
         $id = new TEntry('id');
+        $documento = new TEntry('documento');
         $conta_id = new TDBUniqueSearch('conta_id', 'db_condominio', 'Conta', 'id', 'descricao');
         $conta_id->setMinLength(0);
         $conta_id->setMask('{descricao}');
-        $rateio = new TCombo('rateio');
-        $rateio->addItems(['fracao'=>'fracao','valor' => 'valor']);
-        $valor = new TEntry('valor');
         $data_vencimento = new TDate('data_vencimento');
-        $data_pagamento = new TDate('data_pagamento');
-        $valor_pago = new TEntry('valor_pago');
-        $observacao = new TEntry('observacao');
         $pessoa_id = new TDBUniqueSearch('pessoa_id', 'db_condominio', 'Pessoa', 'id', 'nome');
         $pessoa_id->setMinLength(0);
-        $saldo = new TEntry('saldo');
+        $pessoa_id->setMask('{nome}');
+        $valor = new TEntry('valor');
+        $data_recebimento = new TDate('data_recebimento');
+        $valor_recebido = new TEntry('valor_recebido');
+        $juros_recebido = new TEntry('juros_recebido');
         $status = new TCombo('status');
         $status->addItems(['Liquidado'=>'Liquidado','Pendente' => 'Pendente','Parcelado' => 'Parcelado']);
-
-
+        $observacao = new TEntry('observacao'); 
+    
         $this->form->addFields([ new TLabel('Id')], [$id]);
+        $this->form->addFields([ new TLabel('Documento')], [$documento]);
         $this->form->addFields([ new TLabel('Conta')], [$conta_id]);
-        $this->form->addFields([ new TLabel('Rateio')], [$rateio]);
-        $this->form->addFields([ new TLabel('Valor')], [$valor]);
         $this->form->addFields([ new TLabel('Data Vencimento')], [$data_vencimento]);
-        $this->form->addFields([ new TLabel('Data Pagamento')], [$data_pagamento]);
-        $this->form->addFields([ new TLabel('Valor Pago')], [$valor_pago]);
-        $this->form->addFields([ new TLabel('Observação')], [$observacao]);
         $this->form->addFields([ new TLabel('Pessoa')], [$pessoa_id]);
-        $this->form->addFields([ new TLabel('Saldo')], [$saldo]);
+        $this->form->addFields([ new TLabel('Valor')], [$valor]);
+        $this->form->addFields([ new TLabel('Data Recebimento')], [$data_recebimento]);
+        $this->form->addFields([ new TLabel('Valor Recebido')], [$valor_recebido]);
+        $this->form->addFields([ new TLabel('Juros Recebido')], [$juros_recebido]);
         $this->form->addFields([ new TLabel('Status')], [$status]);
+        $this->form->addFields([ new TLabel('Observação')], [$observacao]);
 
         $data_vencimento->setMask('dd/mm/yyyy');
         $data_vencimento->setDatabaseMask('yyyy-mm-dd');
 
-        $data_pagamento->setMask('dd/mm/yyyy');
-        $data_pagamento->setDatabaseMask('yyyy-mm-dd');
+        $data_recebimento->setMask('dd/mm/yyyy');
+        $data_recebimento->setDatabaseMask('yyyy-mm-dd');
 
         $valor->setNumericMask(2, ',', '.' ,true);
-        $valor_pago->setNumericMask(2, ',', '.' ,true);
-        $saldo->setNumericMask(2, ',', '.' ,true);
+        $valor_recebido->setNumericMask(2, ',', '.' ,true);
         
-
-        $pessoa_id->addValidation('Pessoa', new TRequiredValidator);
+        $documento->addValidation('Documento', new TRequiredValidator);
         $conta_id->addValidation('Conta', new TRequiredValidator);
-        $rateio->addValidation('Rateio', new TRequiredValidator);
+        $pessoa_id->addValidation('Pessoa', new TRequiredValidator);
         $valor->addValidation('Valor', new TRequiredValidator);
         $data_vencimento->addValidation('Data Vencimento', new TRequiredValidator);
 
         $id->setSize('100%');
+        $documento->setSize('100%');
         $conta_id->setSize('100%');
-        $rateio->setSize('100%');
-        $valor->setSize('100%');
         $data_vencimento->setSize('100%');
-        $data_pagamento->setSize('100%');
-        $valor_pago->setSize('100%');
-        $observacao->setSize('100%');
         $pessoa_id->setSize('100%');
-        $saldo->setSize('100%');
+        $valor->setSize('100%');
+        $data_recebimento->setSize('100%');
+        $valor_recebido->setSize('100%');
+        $juros_recebido->setSize('100%');
         $status->setSize('100%');
+        $observacao->setSize('100%');
 
         $id->setEditable(FALSE);
         
--- app/control/financeiro/ContaReceberList.php
+++ app/control/financeiro/ContaReceberList.php
@@ -17,7 +17,7 @@ use Adianti\Widget\Wrapper\TDBUniqueSearch;
 use Adianti\Wrapper\BootstrapDatagridWrapper;
 use Adianti\Wrapper\BootstrapFormBuilder;
 
-class ContaPagarList extends TPage
+class ContaReceberList extends TPage
 {
     protected $form;
     protected $datagrid;
@@ -32,20 +32,21 @@ class ContaPagarList extends TPage
         parent::__construct();
 
         $this->setDatabase('db_condominio');
-        $this->setActiveRecord('ContaPagar');
+        $this->setActiveRecord('ContaReceber');
         $this->setDefaultOrder('id', 'asc');
-        $this->setOrderCommand('pessoa->nome','(SELECT nome FROM pessoa WHERE id=conta_pagar.pessoa_id');
+        $this->setOrderCommand('pessoa->nome','(SELECT nome FROM pessoa WHERE id=conta_receber.pessoa_id');
         $this->setLimit(10);
 
         $this->addFilterField('id', '=', 'id');
         $this->addFilterField('conta_id', '=', 'conta_id');
         $this->addFilterField('pessoa_id', 'like', 'pessoa_id');
+        $this->addFilterField('documento', 'like', 'documento');
         $this->addFilterField('status', 'like', 'status');
         
         
 
-        $this->form = new BootstrapFormBuilder('form_search_ContaPagar');
-        $this->form->setFormTitle('Contas a Pagar');
+        $this->form = new BootstrapFormBuilder('form_search_ContaReceber');
+        $this->form->setFormTitle('Contas a Receber');
 
         $id = new TEntry('id');
         $conta_id = new TDBUniqueSearch('conta_id', 'db_condominio', 'Conta', 'id', 'descricao');
@@ -54,23 +55,26 @@ class ContaPagarList extends TPage
         $pessoa_id = new TDBUniqueSearch('pessoa_id', 'db_condominio', 'Pessoa', 'id', 'nome');
         $status = new TCombo('status');
         $status->addItems(['Liquidado'=>'Liquidado','Pendente' => 'Pendente','Parcelado' => 'Parcelado']);
+        $documento = new TEntry('documento');
         
 
         $this->form->addFields([ new TLabel('Id') ], [ $id]);
         $this->form->addFields([ new TLabel('Conta') ], [$conta_id]);
         $this->form->addFields([ new TLabel('Pessoa') ], [$pessoa_id]);
         $this->form->addFields([ new TLabel('Status') ], [ $status]);
+        $this->form->addFields([ new TLabel('Documento') ], [ $documento]);
 
         $id->setSize('30%');
         $conta_id->setSize('100%');
         $pessoa_id->setSize('100%');
         $status->setSize('100%');
+        $documento->setSize('100%');
 
         $this->form->setData( TSession::getValue(__CLASS__.'_filter_data') );
 
         $btn = $this->form->addAction(_t('Find'), new TAction([ $this, 'onSearch']), 'fa:search');
         $btn->class = 'btn btn-sm btn-primary';
-        $this->form->addActionLink(_t('New'), new TAction(['ContaPagarForm', 'onEdit'], ['register_state' => 'false']), 'fa:plus green');
+        $this->form->addActionLink(_t('New'), new TAction(['ContaReceberForm', 'onEdit'], ['register_state' => 'false']), 'fa:plus green');
 
         //Cria datagrid
         $this->datagrid = new BootstrapDatagridWrapper(new TDataGrid);
@@ -80,29 +84,29 @@ class ContaPagarList extends TPage
 
         //Criar as colunas
         $column_id = new TDataGridColumn('id', 'Id', 'left', '10%');
+        $column_documento = new TDataGridColumn('documento', 'Documento', 'left');
         $column_conta_id = new TDataGridColumn('{conta->descricao}', 'Conta', 'left');
-        $column_rateio = new TDataGridColumn('rateio', 'Rateio', 'left');
-        $column_valor = new TDataGridColumn('valor', 'Valor', 'left');
         $column_data_vencimento = new TDataGridColumn('data_vencimento', 'Data Vencimento', 'left');
-        $column_data_pagamento = new TDataGridColumn('data_pagamento', 'Data Pagamento', 'left');
-        $column_valor_pago = new TDataGridColumn('valor_pago', 'Valor Pago', 'left');
-        $column_observacao = new TDataGridColumn('observacao', 'Observação', 'left');
         $column_pessoa_id = new TDataGridColumn('{pessoa->nome}', 'Pessoa', 'left');
-        $column_saldo = new TDataGridColumn('saldo', 'Saldo', 'left');
+        $column_valor = new TDataGridColumn('valor', 'Valor', 'left');
+        $column_data_recebimento = new TDataGridColumn('data_recebimento', 'Data Recebimento', 'left');
+        $column_valor_recebido = new TDataGridColumn('valor_recebido', 'Valor Recebido', 'left');
+        $column_juros_recebido = new TDataGridColumn('juros_recebido', 'Juros Recebido', 'left');
         $column_status = new TDataGridColumn('status', 'Status', 'left');
+        $column_observacao = new TDataGridColumn('observacao', 'Observação', 'left');
 
         $this->datagrid->addColumn($column_id);
+        $this->datagrid->addColumn($column_documento);
         $this->datagrid->addColumn($column_conta_id);
-        $this->datagrid->addColumn($column_rateio);
-        $this->datagrid->addColumn($column_valor);
         $this->datagrid->addColumn($column_data_vencimento);
-        $this->datagrid->addColumn($column_data_pagamento);
-        $this->datagrid->addColumn($column_valor_pago);
-        $this->datagrid->addColumn($column_observacao);
         $this->datagrid->addColumn($column_pessoa_id);
-        $this->datagrid->addColumn($column_saldo);
+        $this->datagrid->addColumn($column_valor);
+        $this->datagrid->addColumn($column_data_recebimento);
+        $this->datagrid->addColumn($column_valor_recebido);
+        $this->datagrid->addColumn($column_juros_recebido);
+        $this->datagrid->addColumn($column_observacao);
         $this->datagrid->addColumn($column_status);
-
+        
         $column_status->setTransformer(function($value, $object, $row) { 
             $lbl = new TLabel(''); 
             if ($value == 'Liquidado') { 
@@ -128,24 +132,24 @@ class ContaPagarList extends TPage
         };
 
         $column_valor->setTransformer($format_value);
-        $column_valor_pago->setTransformer($format_value);
-        $column_saldo->setTransformer($format_value);
+        $column_valor_recebido->setTransformer($format_value);
+        $column_juros_recebido->setTransformer($format_value);
 
         $column_id->setAction(new TAction([$this, 'onReload']), ['order' => 'id']);
         $column_conta_id->setAction(new TAction([$this, 'onReload']), ['order' => 'conta_id']);
         $column_pessoa_id->setAction(new TAction([$this, 'onReload']), ['order' => 'pessoa_id']);
-
-        $column_data_pagamento->setTransformer( function($value, $object, $row) {
-            $date = new DateTime($value);
-            return $date->format('d/m/Y');
-        });
 
         $column_data_vencimento->setTransformer( function($value, $object, $row) {
             $date = new DateTime($value);
             return $date->format('d/m/Y');
         });
 
-        $action1 = new TDataGridAction(['ContaPagarForm', 'onEdit'], ['id' => '{id}', 'register_state' => 'false']);
+        $column_data_recebimento->setTransformer( function($value, $object, $row) {
+            $date = new DateTime($value);
+            return $date->format('d/m/Y');
+        });
+
+        $action1 = new TDataGridAction(['ContaReceberForm', 'onEdit'], ['id' => '{id}', 'register_state' => 'false']);
         $action2 = new TDataGridAction([$this, 'onDelete'], ['id' => '{id}']);
 
         $this->datagrid->addAction($action1, _t('Edit'), 'fa:edit blue');
